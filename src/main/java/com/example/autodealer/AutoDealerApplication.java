ea8737a739bf32be952da9f1015bff9cc7f21fbb
tiberiup94 <tiberiu.pauna@yahoo.com>
2024-06-18 19:54:15 +0300

    create project

---
+++ src/main/java/com/example/autodealer/AutoDealerApplication.java
@@ -0,0 +1,13 @@
+package com.example.autodealer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoDealerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoDealerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/autodealer/AutoDealerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autodealer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoDealerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
